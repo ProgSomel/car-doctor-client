ef0ee0139495748f995cfae506d5b9287fd026c2
Somel <somelahmed88@gmail.com>
2023-11-02 12:58:12 +0600

    Create About section with Image Relative Position

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -7,13 +7,13 @@ import img6 from '../../../assets/images/banner/6.jpg'
 
 const Banner = () => {
   return (
-    <div className="carousel w-full">
+    <div className="carousel w-full space-y-7 mt-5">
       <div id="slide1" className="carousel-item relative w-full h-[600px] ">
         <img
           src={img1}
           className="w-full rounded-lg"
         />
-        <div className="absolute flex items-center h-full     left-0  top-0 bottom-0 gap-5 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00) ]">
+        <div className="absolute flex items-center h-full     left-0  top-0 bottom-0 gap-5 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00) ] rounded-lg">
           <div className='text-white space-y-7 w-1/2 pl-16'>
             <h2 className='text-6xl font-bold'>Affordable
              Price For Car Servicing</h2>
@@ -33,11 +33,22 @@ const Banner = () => {
           </a>
         </div>
       </div>
-      <div id="slide2" className="carousel-item relative w-full">
-        <img
+      <div id="slide2" className="carousel-item relative w-full h-[600px] ">
+      <img
           src={img2}
-          className="w-full"
+          className="w-full rounded-lg"
         />
+        <div className="absolute flex items-center h-full     left-0  top-0 bottom-0 gap-5 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00) ] rounded-lg">
+          <div className='text-white space-y-7 w-1/2 pl-16'>
+            <h2 className='text-6xl font-bold'>Affordable
+             Price For Car Servicing</h2>
+            <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+            <div className='flex items-center'>
+            <button className="btn btn-primary">Discover More</button>
+            <button className="btn btn-outline btn-secondary ml-5">Latest Projects</button>
+            </div>
+          </div>
+        </div>
         <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0 gap-5">
           <a href="#slide1" className="btn btn-circle">
             ❮
@@ -47,11 +58,22 @@ const Banner = () => {
           </a>
         </div>
       </div>
-      <div id="slide3" className="carousel-item relative w-full">
-        <img
+      <div id="slide3" className="carousel-item relative w-full h-[600px] ">
+      <img
           src={img3}
-          className="w-full"
+          className="w-full rounded-lg"
         />
+        <div className="absolute flex items-center h-full     left-0  top-0 bottom-0 gap-5 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00) ] rounded-lg">
+          <div className='text-white space-y-7 w-1/2 pl-16'>
+            <h2 className='text-6xl font-bold'>Affordable
+             Price For Car Servicing</h2>
+            <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+            <div className='flex items-center'>
+            <button className="btn btn-primary">Discover More</button>
+            <button className="btn btn-outline btn-secondary ml-5">Latest Projects</button>
+            </div>
+          </div>
+        </div>
         <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0 gap-5">
           <a href="#slide2" className="btn btn-circle">
             ❮
@@ -61,11 +83,22 @@ const Banner = () => {
           </a>
         </div>
       </div>
-      <div id="slide4" className="carousel-item relative w-full">
-        <img
+      <div id="slide4" className="carousel-item relative w-full h-[600px] ">
+      <img
           src={img4}
-          className="w-full"
+          className="w-full rounded-lg"
         />
+        <div className="absolute flex items-center h-full     left-0  top-0 bottom-0 gap-5 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00) ] rounded-lg">
+          <div className='text-white space-y-7 w-1/2 pl-16'>
+            <h2 className='text-6xl font-bold'>Affordable
+             Price For Car Servicing</h2>
+            <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+            <div className='flex items-center'>
+            <button className="btn btn-primary">Discover More</button>
+            <button className="btn btn-outline btn-secondary ml-5">Latest Projects</button>
+            </div>
+          </div>
+        </div>
         <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0 gap-5">
           <a href="#slide3" className="btn btn-circle">
             ❮
