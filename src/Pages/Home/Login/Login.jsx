11a87b7ebcd3f76a0f5396d1b6870f74d4f73c3d
Somel <somelahmed88@gmail.com>
2023-11-06 12:37:09 +0600

    custom hook added

--- src/Pages/Home/Login/Login.jsx
+++ src/Pages/Home/Login/Login.jsx
@@ -1,12 +1,14 @@
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import img from '../../../assets/images/login/login.svg'
-import { useContext } from 'react';
-import { AuthContext } from '../../../Provider/AuthProvider';
+
 import axios from 'axios';
+import useAuth from '../../../hooks/useAuth';
 
 const Login = () => {
 
-    const {signIn} = useContext(AuthContext);
+    // const {signIn} = useContext(AuthContext);
+    const {signIn} = useAuth();
+    
     
 
     const location = useLocation();
